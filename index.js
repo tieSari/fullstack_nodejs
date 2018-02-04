d6fd5ed4e9236e8dae0e88826b85079f67a8f60e
Sari R <sariraut@dhcp-asv-101.helsinki.fi>
2018-02-04 20:32:56 +0200

    teht3_3

--- index.js
+++ index.js
@@ -24,6 +24,17 @@ app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if ( person ) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
 app.get('/info', (req, res) => {
   res.send('<div>puhelinluettelossa '+persons.length +' henkilön tiedot</div>' +
   '<div>' + new Date() +'</div>')
