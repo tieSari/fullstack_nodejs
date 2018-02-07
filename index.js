ec6fe45ccbb4abc8ea44b615dba04c84a8f4a02c
Sari R <sariraut@dhcp-asv-101.helsinki.fi>
2018-02-07 20:53:17 +0200

    Initiate app

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ app.get('/info', (req, res) => {
   '<div>' + new Date() +'</div>')
 })
 
-const PORT = 3003
+const PORT = process.env.PORT || 3003
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
