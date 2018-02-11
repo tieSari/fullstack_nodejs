4df8a293c572072ca701e1844783dbc5d962f92a
Sari R <sariraut@dhcp-asv-101.helsinki.fi>
2018-02-11 10:18:45 +0200

    teht3_22

--- models/person.js
+++ models/person.js
@@ -3,9 +3,9 @@
 const mongoose = require('mongoose')
 console.log(process.env.NODE_ENV)
 
-if ( process.env.NODE_ENV === 'undefined' || process.env.NODE_ENV !== 'production' ) {
+if (process.env.NODE_ENV === 'undefined' || process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
-  }
+}
 const url = process.env.MONGODB_URI
 //console.log(url)
 
@@ -16,24 +16,22 @@ var Schema = mongoose.Schema
 
 
 const personSchema = new Schema({
-    name:   String,
+    name: String,
     number: String
 })
 
- personSchema.statics.format = function(person)
- {
-     const formattedPerson = { number: person.number, name: person.name, id: person._id }
-     return formattedPerson
- }
+personSchema.statics.format = function (person) {
+    const formattedPerson = { number: person.number, name: person.name, id: person._id }
+    return formattedPerson
+}
 
- personSchema.query.byName = function(name)
- {
-     return this.find({name: new RegExp(name, 'i')})
- }
+personSchema.query.byName = function (name) {
+    return this.find({ name: new RegExp(name, 'i') })
+}
 
 const Person = mongoose.model('Person', personSchema);
 
 
 
- 
-   module.exports = Person
+
+module.exports = Person
